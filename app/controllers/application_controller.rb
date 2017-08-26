97db620a52aa895d454458c85ac1633f69425399
Marihuana <aisatu_nomahou@yahoo.co.jp>
2017-08-27 03:22:21 +0900

    APIサーバアクセス時にaccess_tokenの認証を行うようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  before_action :authentication
+
+  def authentication
+    return unless enable_auth?
+    session = Session.find_by(access_token: params[:access_token])
+    render status: :unauthorized and return if session.blank?
+  end
+
+  # 認証しない場合はこれをオーバーライドする
+  def enable_auth?
+    true
+  end
 end
