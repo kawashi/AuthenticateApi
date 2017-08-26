1ac19c77df4fb742c6aa6b2657b7c9460d81fd2e
Marihuana <aisatu_nomahou@yahoo.co.jp>
2017-08-26 14:39:14 +0900

    死活監視用のControllerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home', to: 'home#index', format: 'json'
+
   post 'login', to: 'login#index', format: 'json'
   post 'sign_up', to: 'user#create', format: 'json'
   post 'auth_check', to: 'login#check', format: 'json'
